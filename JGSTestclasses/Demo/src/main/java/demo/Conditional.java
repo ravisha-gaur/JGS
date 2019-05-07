565fb7cce1f747b0d0d7dcc6fe3396bda76221e2
Ravisha Gaur <ravisha0212@gmail.com>
2019-05-07 10:08:49 +0200

    update demo conditional

--- JGSTestclasses/Demo/src/main/java/demo/Conditional.java
+++ JGSTestclasses/Demo/src/main/java/demo/Conditional.java
@@ -9,26 +9,35 @@ public class Conditional {
     public static void main(String[] args) {
 
         int x = Casts.cast("LOW ~> ?", 7);
-        //int y = Casts.cast("LOW ~> ?", 3);
+        //boolean a = Casts.cast("LOW ~> ?", true);
+        int y = Casts.cast("LOW ~> ?", 3);
 
        //int x = 7;
        //int y = 7;
 
-        if (x > 5) {
+
+        //if(a) {
+        if (x > 5 && y < 10) {
             x = 0;
+            x = DynamicLabel.makeHigh(5);
+            y = 100;
             /*if(y > 6) {  // if (x < 0) {
                 y = 0;
             }*/
         }
-        else if(x > 10) {
+        else if(x > 3) {
+            x = 10;
+            //y = 0;
+        }
+        else if(x > 3) {
             x = 100;
+            y = 0;
         }
         else{				// Tricky: b1 is uninitialized, throws IlFlowEx
-            x = 6;
+            x = 500;
         }
-
-        IOUtils.printSecret(x);
-        //IOUtils.printSecret(y);
+        //IOUtils.printSecret(x);
+        IOUtils.printSecret(y);
 
      /*
      * if (x < 0) {			// if (x < 0) {
