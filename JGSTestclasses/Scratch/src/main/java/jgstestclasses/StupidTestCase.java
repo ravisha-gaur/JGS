8c0403f16f73c57ac7d711be221dd3e359af720a
Ravisha Gaur <ravisha0212@gmail.com>
2019-07-17 13:11:44 +0200

    unusual test case where method args are independent of the return var

--- JGSTestclasses/Scratch/src/main/java/jgstestclasses/StupidTestCase.java
+++ JGSTestclasses/Scratch/src/main/java/jgstestclasses/StupidTestCase.java
@@ -1,4 +1,30 @@
 package jgstestclasses;
 
+import de.unifreiburg.cs.proglang.jgs.support.*;
+
 public class StupidTestCase {
+
+    @Sec("?")
+    static int z;
+
+    @Constraints("LOW <= @0")
+    @Effects({"HIGH", "?", "LOW"})
+    public static void main(String[] args) {
+
+        int r = Casts.cast("HIGH ~> ?", 70);
+        int a = Casts.cast("? ~> HIGH", 999);
+        int s = stupid(a, r);
+        IOUtils.printSecret(z);
+        IOUtils.printSecret(s);
+    }
+
+    @Constraints({"@0 <= @ret", "@0 <= HIGH", "@1 <= ?"})
+    @Effects({"HIGH", "?"})
+    public static int stupid(int x, int y){
+
+        z = y;
+        return x+1;
+
+    }
+
 }
